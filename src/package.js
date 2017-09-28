a3e10e4a420c339fc55f9177fe85d53148e3d438
Abel Varga <rangeoshun@gmail.com>
2017-09-28 20:09:12 +0200

    Add custom package infos for self publish until merge

--- src/package.js
+++ src/package.js
@@ -1,26 +1,28 @@
-'use strict';   // eslint-disable-line strict, lines-around-directive
+"use strict" // eslint-disable-line strict, lines-around-directive
 
-const name = 'ssrwpo:ssr';
+const name = "rangeoshun:ssr"
 Package.describe({
   name,
-  version: '2.6.0',
-  summary: 'SSR - Router with SSR for Node & Meteor',
-  git: 'https://github.com/ssr-server/ssr',
-  documentation: 'README.md',
-});
+  version: "2.6.0",
+  summary: "SSR - Router with SSR for Node & Meteor",
+  git: "https://github.com/rangeoshun/ssr",
+  documentation: "README.md"
+})
 
 // Packages used on client and server
 const sharedPkgs = [
   // MDG packages
-  'ecmascript', 'ejson', 'webapp',
+  "ecmascript",
+  "ejson",
+  "webapp",
   // Community packages
-  'tmeasday:check-npm-versions@0.3.1',
-];
+  "tmeasday:check-npm-versions@0.3.1"
+]
 
-Package.onUse((api) => {
-  api.versionsFrom('1.4.4.2');
-  api.use(sharedPkgs);
-  api.mainModule('shared/utils/peerDependencies.js', ['client', 'server']);
-  api.mainModule('client/index.js', 'client');
-  api.mainModule('server/index.js', 'server');
-});
+Package.onUse(api => {
+  api.versionsFrom("1.4.4.2")
+  api.use(sharedPkgs)
+  api.mainModule("shared/utils/peerDependencies.js", ["client", "server"])
+  api.mainModule("client/index.js", "client")
+  api.mainModule("server/index.js", "server")
+})
